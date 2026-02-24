9d7066d19522c83c89bb975363c78b634001629c
Valeria Gabrielli <gabvale53@gmail.com>
2026-02-24 02:03:46 +0100

    feat: aggiunto reviews fix : spostato il res.json all'interno della callback

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -19,16 +19,25 @@ function show(req, res) {
     const { id } = req.params;
     //preparo query per richiesta
     const moviesSql = 'SELECT * FROM movies WHERE id = ?';
-
+    
+    const movieReviews = 'SELECT * FROM reviews WHERE movie_id = ?';
+    //chiamata a DB per recuperare film
     connection.query(moviesSql, [id], (err, results) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
         if (results.length === 0) return res.status(404).json({ error: 'Error, movie not found' })
 
             //salvo il risultato
             const movie = results[0];
-            
-            //ritorno il json di movies
-            res.json(movie);
+            //chiamata a DB per reviews
+            connection.query(movieReviews,[id],(err, reviewResults)=>{
+                if (err) return res.status(500).json({ error:'Database query failed'});
+                //salvo le recensioni in una const 
+                const reviewsArray = reviewResults;
+                //aggiungo la prop per le reviews
+                movie.reviews = reviewsArray;
+                //ritorno il json di movies
+                res.json(movie);
+            })
     });
 }
 
